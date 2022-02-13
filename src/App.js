667793719b7adbc5a34effe4df41a9b6b47eaab5
Tampoka <goal.thanks@gmail.com>
2022-02-13 10:47:26 +0900

    Add ColorsList, Color.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Menu from './components/Menu';
 import data from "./data/recipes.json";
+import colorData from "./data/color-data.json";
+import {useState} from 'react';
+import ColorList from './components/ColorsList';
 
 function App() {
-  return (
-    <div className="App">
-      <Menu recipes={data}/>
-    </div>
-  );
+    const [colors, setColors] = useState(colorData)
+
+    return (
+        <div className="App">
+            <ColorList colors={colors}/>
+             {/*<Menu recipes={data}/>*/}
+        </div>
+    )
 }
 
 export default App;
--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -3,15 +3,15 @@ import {Star} from './Star';
 
 const createArray = length => [...Array(length)]
 
-export default function StarRating({style={},totalStars = 5,...props}) {
-    const [selectedStars, setSelectedStars] = useState(3)
+export default function StarRating({style={},totalStars = 5,selectedStars=0,...props}) {
     return (
         <div style={{padding:"5px",...style}} {...props}>
             {createArray(totalStars).map((n, i) => (
                 <Star
                     key={i}
                     selected={selectedStars > i}
-                onSelect={()=>setSelectedStars(i+1)}/>
+                // onSelect={()=>setSelectedStars(i+1)}
+                />
             ))}
             <p>
                 {selectedStars} of {totalStars} stars
