451de169136e64acf53f0af4b20fdc7e56e6a9ce
Tampoka <goal.thanks@gmail.com>
2022-02-15 23:10:20 +0900

    Add UserRepositories.

---
+++ src/customHooks/useJazzyNews.js
@@ -0,0 +1,23 @@
+import {useEffect, useMemo, useState} from 'react';
+
+const useJazzyNews = () => {
+    const [_posts, setPosts] = useState([]);
+    const addPost = post => setPosts(allPosts => [post, ...allPosts]);
+    const posts = useMemo(() => _posts, [_posts]);
+
+    useEffect(() => { //every time new post added
+        newPostChime.play();
+    }, [posts]);
+
+    useEffect(() => {
+        newsFeed.subscribe(addPost);
+        return () => newsFeed.unsubscribe(addPost);
+    }, []);
+
+    useEffect(() => {
+        welcomeChime.play();
+        return () => goodbyeChime.play();
+    }, []);
+
+    return posts;
+};
