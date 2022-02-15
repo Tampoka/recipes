f33d379c37b7965a5e65cba3263c038d3ca99892
Tampoka <goal.thanks@gmail.com>
2022-02-15 22:43:11 +0900

    Add RepoMenu.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import RepoMenu from './components/RepoMenu';
 
 function App() {
     return (
         <div className="App">
-
+            <RepoMenu/>
         </div>
     )
 }
