8ee45737673d490a7904866b5a0bc842b2877e2c
Tampoka <goal.thanks@gmail.com>
2022-02-19 17:56:44 +0900

    Add jsx-a11y plugin.

--- src/components/sample.js
+++ src/components/sample.js
@@ -1,3 +1,4 @@
+/*
 const gnar = "gnarly";
 const info = ({
                   file = __filename,
@@ -11,4 +12,13 @@ switch (gnar) {
     default:
         console.log("gnarly");
         break;
+}*/
+
+import {useState} from 'react';
+
+function gnar() {
+    const [nickname, setNickname] = useState(
+        "dude"
+    );
+    return <h1>gnarly</h1>;
 }
