672671127353fda36930a720870627d4ac58fbd0
Tampoka <goal.thanks@gmail.com>
2022-03-11 11:02:33 +0900

    Add foundColor.

--- src/components/Context/ColorDetails.js
+++ src/components/Context/ColorDetails.js
@@ -1,7 +1,16 @@
+import { useParams } from 'react-router';
+import { useColors } from '../../customHooks/color-hooks';
+
 export function ColorDetails() {
+  let { id } = useParams();
+  let { colors } = useColors();
+
+  let foundColor = colors.find((color) => color.id === id);
+  const { title, rating } = foundColor;
   return (
     <div>
-      <h1>Details</h1>
+      <h1>{title}</h1>
+      <h3>Rating: {rating}</h3>
     </div>
   );
 }
--- src/components/UseReducer/Checkbox.js
+++ src/components/UseReducer/Checkbox.js
@@ -9,11 +9,12 @@ export default function Checkbox() {
   );
   // const [first, setFirst] = useState(true)
   // const [number, setNumber]=useReducer((number,newNumber)=>number+newNumber,10,n=>n)
-  const [number, setNumber] = useReducer(
+
+  /*  const [number, setNumber] = useReducer(
     (number, newNumber) => number + newNumber,
     10,
     (n) => n * 2,
-  );
+  );*/
 
   // const onCheck = () => {
   //     setChecked(checked=>!checked)
@@ -32,9 +33,10 @@ export default function Checkbox() {
       {checked ? 'checked' : 'not checked'}
       <hr />
       {/* eslint-disable-next-line jsx-a11y/aria-role,jsx-a11y/click-events-have-key-events */}
-      <div onClick={() => setNumber(30)} role="command">
+
+      {/*   <div onClick={() => setNumber(30)} role="command">
         {number}
-      </div>
+      </div>*/}
     </>
   );
 }
