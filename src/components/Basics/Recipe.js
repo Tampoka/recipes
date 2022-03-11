d61f69fe44f31576ca5a117b7fce6d2c9988bdb6
Tampoka <goal.thanks@gmail.com>
2022-03-11 17:15:00 +0900

    Add Menu.

--- src/components/Basics/Recipe.js
+++ src/components/Basics/Recipe.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IngredientsList from './IngredientsList';
 import Instructions from './Instructions';
 import StarRating from './StarRating';
 
 function Recipe({ name, ingredients, steps }) {
+  const [rating, setRating] = useState(0);
   return (
     <section id={name.toLowerCase().replace(/ /g, '-')}>
       <h1>{name}</h1>
-      <StarRating style={{ backgroundColor: 'lightblue' }} />
+      <StarRating
+        style={{ backgroundColor: 'lightblue' }}
+        onRate={setRating}
+        selectedStars={rating}
+      />
       <IngredientsList list={ingredients} />
       <Instructions title="Cooking Instructions" steps={steps} />
     </section>
